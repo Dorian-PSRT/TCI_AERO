f7d66f4849700c8a4b3fbd1445155dfae01cf89b
Nawel <dnawel.r@gmail.com>
2025-11-05 11:27:02 +0000

    ajout Swarm2

---
+++ ws_crazyDev/src/CrazyTest/CrazyTest/CrazySwarm.py
@@ -0,0 +1,8 @@
+import rclpy
+import time
+from rclpy.node import Node 
+import cflib.crtp
+from cflib.crazyflie import Crazyflie
+from rclpy.executors import MultiThreadedExecutor
+from crazyflie_py import Crazyswarm
+
---
+++ ws_crazyDev/src/Swarmtest/setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+from glob import glob
+import os
+
+package_name = 'Swarmtest'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*')),
+        (os.path.join('share', package_name, 'config'), glob('config/*'))
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='etu.tci',
+    maintainer_email='dnawel.r@gmail.com',
+    description='TODO: Package description',
+    license='MIT',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'node1 = Swarmtest.SimpleTest:main'
+        ],
+    },
+)
