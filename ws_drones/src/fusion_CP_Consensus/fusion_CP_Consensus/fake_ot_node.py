2d8e09c562df86a1f5b1c8ba8fdf72e1bf1802e1
Dimitri Nirpot <dimitri.nirpot@gmail.com>
2025-11-10 18:46:26 +0000

    fake_ot_node terminée, reste à l'intégrer dans les autres nodes

--- ws_drones/src/fusion_CP_Consensus/fusion_CP_Consensus/fake_ot_node.py
+++ ws_drones/src/fusion_CP_Consensus/fusion_CP_Consensus/fake_ot_node.py
@@ -2,11 +2,10 @@
 import rclpy
 from rclpy.node import Node
 #import des types qui serviront aux topics
-from geometry_msgs.msg import Twist
+from my_custom_interfaces.msg import PosObstacles
 from turtlesim.msg import Pose
 #import de bibliotheques ou classes pour des besoins spécifiques
-from fusion_CP_Consensus.pid_class import PID #on importe la classe dans laquelle on a délocalisé le calcul de PID
-import math
+
 import json
 from pathlib import Path
 
@@ -24,24 +23,32 @@ class fake_ot_node(Node):
     def __init__(self):
         super().__init__('fake_ot_node')
 
-        self.obstacles_flotants = []
+        self.obstacles_flotants = [(100,100) for _ in range(nb_drones)]
         self.obstacles_fixes    = []
 
-        self.subscription1 = self.create_subscription(Pose,'/turtle1/pose', self.pose(1),10)
-        self.subscription2 = self.create_subscription(Pose,'/turtle2/pose', self.pose(2),10)
-        self.subscription3 = self.create_subscription(Pose,'/turtle3/pose', self.pose(3),10)
-        self.subscription4 = self.create_subscription(Pose,'/turtle4/pose', self.pose(4),10)
+        self.subscription1 = self.create_subscription(Pose,'/turtle1/pose', self.pose1,10)
+        self.subscription2 = self.create_subscription(Pose,'/turtle2/pose', self.pose2,10)
+        self.subscription3 = self.create_subscription(Pose,'/turtle3/pose', self.pose3,10)
+        self.subscription4 = self.create_subscription(Pose,'/turtle4/pose', self.pose4,10)
         self.timer         = self.create_timer(0.1, self.send_info)
 
-        self.publisher = self.create_publisher(Pose,'OptiTrack/obstacles',10)
+        self.publisher = self.create_publisher(PosObstacles,'OptiTrack/obstacles',10)
 
         self.get_logger().info('Le nœud émulateur Opti Track est démarré !')
 
-    def pose (self,i,pos):
+    def pose1 (self,pos): self.position (pos,1)
+    def pose2 (self,pos): self.position (pos,2)
+    def pose3 (self,pos): self.position (pos,3)
+    def pose4 (self,pos): self.position (pos,4)
+
+    def position (self,pos,i):
         self.obstacles_flotants[i-1]=(pos.x,pos.y)
 
     def send_info (self):
-        self.publisher.publish(self.obstacles_flotants)
+        obstacles          = PosObstacles()
+        obstacles.fixes    = self.obstacles_fixes
+        obstacles.flotants = self.obstacles_flotants
+        self.publisher.publish(obstacles)
         
             
             
