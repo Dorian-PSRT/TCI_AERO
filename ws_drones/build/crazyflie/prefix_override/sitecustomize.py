a8bce11aa7ed669301b5f5c755959a42cb58fb7f
Nawel <dnawel.r@gmail.com>
2025-11-18 17:07:32 +0100

    Remove build/install/log from tracking

--- ws_drones/build/crazyflie/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/alexmin9/Desktop/TCI_AERO/ws_drones/install/crazyflie'
--- ws_drones/build/my_custom_interfaces/my_custom_interfaces__py/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/alexmin9/Desktop/TCI_AERO/ws_drones/build/my_custom_interfaces/my_custom_interfaces__py
-# Build directory: /home/alexmin9/Desktop/TCI_AERO/ws_drones/build/my_custom_interfaces/my_custom_interfaces__py
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
