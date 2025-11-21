4de667cd5c0a73951b90aaf7801739cf98bd59e2
tschweitzer57 <thibault.schweitzer@estaca.eu>
2025-11-21 20:11:05 +0100

    added pkg control node qui marche

---
+++ AUTRES DOSSIERS/ws_crazyDev/src/crazyflie_control_latest/setup.py
@@ -0,0 +1,35 @@
+from setuptools import find_packages, setup
+import glob
+
+package_name = 'crazyflie_control_latest'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/launch', ['launch/launch.py', 'launch/takeoff.launch.py']  ),
+        ('share/' + package_name + '/config', ['config/params.yaml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='estacalab',
+    maintainer_email='estacalab@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'crazyflie_control_1= crazyflie_control_latest.crazyflie_control_node:main',
+            'takeoff_client= crazyflie_control_latest.takeoff_client:main',
+            
+        ],
+    },
+)
